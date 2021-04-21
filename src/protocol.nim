7a7293d67fe4c68a749c5d134718903af001a57c
kripsonud <kripsonud@gmail.com>
2021-04-21 23:40:01 +0100

    Add Tests to Protoco Module

--- src/protocol.nim
+++ src/protocol.nim
@@ -9,3 +9,14 @@ proc processMessage*(data: string) : Message =
     let dataJson = parseJson(data)
     result.username = dataJson["username"].getStr("Invalid Username")
     result.message = dataJson["message"].getStr("Invalid Message")
+
+
+when isMainModule:
+    block Test:
+        let msg : string = """{"message":"Hello", "username":"Ebube"}"""
+        let obj : Message = processMessage(msg)
+        let cond = obj.username == "Ebube"
+        let cond2 = obj.message == "Hello"
+        doAssert(cond)
+        doAssert(cond2)
+        echo "Tests Passed!"
